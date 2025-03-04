991976a52be54cbb14fe7d303e5ae5d7d0b8124e
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-04 14:49:34 -0300

    v2.14

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,9 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native'
+import { router } from 'expo-router'
+
+import { Button } from '@/components/button'
 
 export default function Dashboard() {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Dashboard</Text>
+            <Button title="Voltar" onPress= {() => router.back()}/>
         </View>
     );
 }
@@ -13,6 +17,8 @@ export const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        padding: 32,
+        gap: 16,
     },
     title: {
         fontSize: 24,

b4818705ec5049881f4a68bb8397b9cac419b3b1
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-04 13:16:33 -0300

    v1.3

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Index() {
     return (
         <View>
-            <Text>Hello React Native!</Text>
+            <Text style={{color:"red", fontSize: 24}}>Hello React Native!</Text>
         </View>
     )
 }

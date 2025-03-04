2526feebda07540056547e2331777ea6ff313e7e
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-04 13:20:56 -0300

    v1.5

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,15 +2,22 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default function Index() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>Hello React Native!</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        padding: 32,
+        justifyContent: "center",
+        alignItems: "center",
+    },
     title:{
         color:"red", 
         fontSize: 24,
+        fontWeight: "bold",
     }
 })

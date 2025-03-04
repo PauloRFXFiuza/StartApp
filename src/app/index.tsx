a1ba003fea5df6ae3322419a78f0f97ade0a3c71
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-04 13:18:55 -0300

    v1.4

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,16 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function Index() {
     return (
         <View>
-            <Text style={{color:"red", fontSize: 24}}>Hello React Native!</Text>
+            <Text style={styles.title}>Hello React Native!</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    title:{
+        color:"red", 
+        fontSize: 24,
+    }
+})

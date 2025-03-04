d38472ddb7b13ee0b64ca07e502a79302ce3f1d5
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-04 13:25:11 -0300

    v1.8

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Index() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Hello React Native!</Text>
+            <Text style={styles.title}>Paulo Fiuza, Full Stack Developer</Text>
         </View>
     )
 }

57eb2b217b6781241fdde3dcdb987d36c7126a28
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-04 13:14:27 -0300

    v1.2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function Index() {
     return (
         <View>
-            <Text>Hello World!</Text>
+            <Text>Hello React Native!</Text>
         </View>
     )
 }

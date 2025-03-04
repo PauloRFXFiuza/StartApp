95b6dbff04d22b0472805f68e9f73f8e9475a7a0
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-03-04 13:22:19 -0300

    v1.6

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
 
 const styles = StyleSheet.create({
     container:{
+        backgroundColor: "black",
         flex: 1,
         padding: 32,
         justifyContent: "center",
